fecef4d649b613fa71c8d963fc6e4e9bdc035b6c
Valery <valeryyafremau@gmail.com>
2025-11-21 11:11:15 +0400

    FE: Add forms for boards

--- src/pages/Accout.tsx
+++ src/pages/Accout.tsx
@@ -2,24 +2,42 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchMe } from "../redux/actions/index";
 import type { RootState } from "../redux/reducers/rootReducer";
+import { useNavigate } from "react-router-dom";
+import { getBoards } from "../redux/reducers/board.slice";
 
 const Account = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchMe());
+    dispatch(getBoards());
   }, [dispatch]);
-  const { data, loading, error } = useSelector(
-    (state: RootState) => state.me
-  );
+  const { data, loading, error } = useSelector((state: RootState) => state.me);
+  const { boards } = useSelector((state: RootState) => state.boards);
   return (
     <div>
-      <div>{import.meta.env.VITE_MODE}</div>
+      <div className=" text-2xl m-3">{data.name}</div>
+      <ul className="mx-4">
+        <div>
+          My boards
+          <button
+            className="bg-green-500 float-right"
+            onClick={() => {
+              navigate("/board/create");
+            }}
+          >
+            New Board
+          </button>
+        </div>
+        {boards.map((board) => (
+          <li key={board.id} className="m-2 p-2 text-blue-500">
+            <a onClick={() => navigate("/board/" + board.id)}>{board.title}</a>
+          </li>
+        ))}
+      </ul>
       {loading && <div>Loading...</div>}
       {error && <div>{error}</div>}
-      <div>
-        <div>{JSON.stringify(data)}</div>
-      </div>
     </div>
   );
 };
--- src/redux/reducers/rootReducer.ts
+++ src/redux/reducers/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import meReducer from "./meReducer";
 import authReducer from "./authSlice";
+import boardSlice from "./board.slice";
 
 const rootReducer = combineReducers({
     auth: authReducer,
     me: meReducer,
+    boards: boardSlice
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
